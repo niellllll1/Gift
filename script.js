b6ac0e6f101ce9b4b64cc5145cd84d01add73ccf
niellllll1 <basaskrujan@gmail.com>
2025-01-15 21:09:25 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleLetter() {
+
+
+  var loveLetter = document.getElementById('love-letter');
+
+
+
+  loveLetter.classList.toggle('open');
+
+
+
+  var openButton = document.querySelector('.open-button');
+
+  openButton.innerText = loveLetter.classList.contains('open') ? 'Close Letter' : 'Open Letter';
+
+}
+
